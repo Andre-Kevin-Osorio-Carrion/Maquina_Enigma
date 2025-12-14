9a65332e8650d7b08e3b88d53e8f9d76507874a1
Luis-Cordero-Carranco <luis.cordero@estudiant.enti.cat>
2025-12-14 18:21:24 +0100

    rotors.py

--- rotors.py
+++ rotors.py
@@ -89,3 +89,43 @@ def rotor3(resposta):
             posicio1 = (posicio1 + 1) % rang_abcedari
        
         return lista_rotor3
+
+def rotor_invers(missatge_xifrat, nom_fitxer_rotor):            #Lògica inversa genèrica per a un sol rotor.
+    
+    posicio1 = 0
+    missatge_desxifrat = ""
+    rang_abcedari = 26
+
+    with open(nom_fitxer_rotor, "r") as r:                      # Llegeix el cablejat i la posició inicial del fitxer
+            alfabet_rotor = r.readline().strip()                # Cablejat del rotor (Línia 1)
+            p_i = r.readline().strip()                          # Posició inicial (Línia 2)
+
+    posicio1 = ALFABET.find(p_i)                                # Troba la posició inicial (offset) dins l'alfabet
+
+    for lletras_xifrada in missatge_xifrat:                     # Desxifra cada lletra
+        
+        idx_in_rotor = alfabet_rotor.find(lletras_xifrada)      # Trobem l'índex de la lletra xifrada dins del cablejat del rotor
+        
+        if idx_in_rotor == -1:
+            # Si no és una lletra, l'afegim (encara que el teu xifrat només produeix lletres majúscules)
+            missatge_desxifrat += lletras_xifrada
+            
+        else:                                                   # Calculem l'índex de la lletra original (posicio2) amb la fórmula inversa
+            posicio2 = (idx_in_rotor - posicio1 + rang_abcedari) % rang_abcedari
+            
+            missatge_desxifrat += ALFABET[posicio2]             # La lletra desxifrada és la que es troba en posicio2 de l'ALFABET
+
+        posicio1 = (posicio1 + 1) % rang_abcedari               # Movem el rotor (ROTACIÓ)
+
+    return missatge_desxifrat
+
+
+# Funcion inversa per a cada rotor
+def rotor1_invers(missatge_xifrat):
+    return rotor_invers(missatge_xifrat, "Rotor1.txt")
+
+def rotor2_invers(missatge_xifrat):
+    return rotor_invers(missatge_xifrat, "Rotor2.txt")
+
+def rotor3_invers(missatge_xifrat):
+    return rotor_invers(missatge_xifrat, "Rotor3.txt")
